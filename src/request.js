5775a2c1ab3d07740ae7a005f31c1252254728a4
ky__yk <vinaka@ky-pro.local>
2022-02-20 17:09:36 +0900

    first commit

---
+++ src/request.js
@@ -0,0 +1,13 @@
+
+const API_KEY = "0629c2c8213fb3b4397c363db443d226";
+
+export const requests ={
+    feachTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    feachNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    feactTopRated:`/discover/tv?api_key=${API_KEY}&languager=en-us`,
+    feactActionMovies:`/discover/tv?api_key=${API_KEY}&with_genres=28`,
+    feactComedyMovies:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    feactHorrorMovies:`/discover/tv?api_key=${API_KEY}&with_genres=27`,
+    feactRomanceMovies:`/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    feactDocumentMovies:`/discover/tv?api_key=${API_KEY}&with_genres=99`,
+}
